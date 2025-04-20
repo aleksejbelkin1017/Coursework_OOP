04649186ad4f5bbfbfe95013b599c5b84939b85d
aleksejbelkin1017 <aleksejbelkin1017@gmail.com>
2025-04-20 18:22:45 +0300

    modified main.py > add user interaction

--- main.py
+++ main.py
@@ -1,18 +1,83 @@
-import json
+from src.vacancies import Vacancy
+from src.filehandler import JSONFileHandler
+from src.api_interactions import hh_API
 
-from src.head_hunter_api import HeadHunterAPI, BaseHeadHunterAPI
-from abc import abstractmethod
-import requests
-from requests.exceptions import HTTPError, RequestException
 
-hh_api = HeadHunterAPI()
-user_input_keyword = input('Введите ключевое слово для вакансии: ')
-hh_vacancies = hh_api.get_vacancies(user_input_keyword)
+def user_interaction():
+    hh_api = hh_API()
+    file_handler = JSONFileHandler()
 
-with open('data/vacancies_1.json', 'w', encoding='utf-8') as outfile:
-    json.dump(hh_vacancies, outfile, ensure_ascii=False, indent=4)
+    while True:
+        print("\nВыберите действие:")
+        print("1. Поиск вакансий по ключевому слову")
+        print("2. Получить топ N вакансий по зарплате")
+        print("3. Получить вакансии с ключевым словом из файла")
+        print("4. Записать вакансию в файл")
+        print("5. Удалить вакансию из файла")
+        print("6. Выход")
 
-# params = {"text": "python"}
-# vacancies = requests.get("https://api.hh.ru/vacancies", params=params).json()
-#
-# print(vacancies["items"])
+        choice = input("Введите номер действия: ")
+
+        if choice == '1':
+            keyword = input("Введите поисковый запрос: ")
+            cantidad = int(input("Введите количество вакансий для получения: "))
+            vacancies = hh_api.get_vacancies(keyword, cantidad)
+            if vacancies:
+                for vacancy in vacancies:
+                    print(
+                        f"Название: {vacancy['name']}, "
+                        f"Компания: {vacancy['company']}, "
+                        f"Зарплата: {vacancy['salary']}, "
+                        f"Ссылка: {vacancy['url']}")
+            else:
+                print("Вакансии не найдены.")
+
+        elif choice == '2':
+            n = int(input("Введите количество вакансий для получения по зарплате: "))
+            vacancies = file_handler.get_vacancies()
+            sorted_vacancies = sorted(vacancies, key=lambda x: x.get('salary', 0), reverse=True)[:n]
+            if sorted_vacancies:
+                for vacancy in sorted_vacancies:
+                    print(
+                        f"Название: {vacancy['name']}, "
+                        f"Компания: {vacancy['company']}, "
+                        f"Зарплата: {vacancy['salary']}, "
+                        f"Ссылка: {vacancy['url']}")
+            else:
+                print("Вакансии не найдены.")
+
+        elif choice == '3':
+            keyword = input("Введите ключевое слово для поиска в файле: ")
+            vacancies = file_handler.get_vacancies(name=keyword)
+            if vacancies:
+                for vacancy in vacancies:
+                    print(
+                        f"Название: {vacancy['name']}, "
+                        f"Компания: {vacancy['company']}, "
+                        f"Зарплата: {vacancy['salary']}, "
+                        f"Ссылка: {vacancy['url']}")
+            else:
+                print("Вакансии не найдены.")
+
+        elif choice == '4':
+            name = input("Введите название вакансии: ")
+            company = input("Введите название компании: ")
+            salary = float(input("Введите зарплату (если не указана, введите 0): "))
+            url = input("Введите ссылку на вакансию: ")
+            id = int(input("Введите ID вакансии"))
+
+            vacancy = Vacancy(id=id, name=name, company=company, salary=salary, url=url)
+            file_handler.add_vacancy(vacancy.to_dict())
+            print("Вакансия добавлена в файл.")
+
+        elif choice == '5':
+            vacancy_id = input("Введите ID вакансии для удаления: ")
+            file_handler.delete_vacancy(vacancy_id)
+            print(f"Вакансия с ID {vacancy_id} удалена из файла.")
+
+        elif choice == '6':
+            print("Выход из программы.")
+            break
+
+        else:
+            print("Некорректный ввод. Пожалуйста, выберите действие от 1 до 6.")
